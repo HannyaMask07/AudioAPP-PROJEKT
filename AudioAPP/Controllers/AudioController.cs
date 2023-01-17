295d3ccaa81a3ae95acb894eabc0d5137b2e0430
Krzysztof Trytek <krzysztof.trytek07@gmail.com>
2023-01-17 20:59:53 +0100

    Added: -Możliwość dodawania pliku png/jpg etc. -Możliwość dodania pliku mp.3/wav etc. -Możliwośc dodania opisu -Możliwość dodania tytułu -Wyświetlanie dodanych treści -Dodana ikona (nie skończone)

---
+++ AudioAPP/Controllers/AudioController.cs
@@ -0,0 +1,42 @@
+﻿using AudioAPP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AudioApp.Controllers
+{
+    public class AudioController : Controller
+    {
+
+        public static List<Audio> audios = new List<Audio>();
+        public static int count = 0;
+        public IActionResult Index()
+        {
+            return View(audios);
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View("AudioForm");
+        }
+        [HttpPost]
+        public IActionResult Add(Audio audio, [FromForm(Name = "image")] IFormFile image, [FromForm(Name = "sound")] IFormFile sound)
+        {
+            if (sound != null)
+            {
+                audio.Sound = new byte[sound.Length];
+                sound.OpenReadStream().Read(audio.Sound, 0, (int)sound.Length);
+            }
+
+            if (image is not null)
+            {
+                audio.Image = new byte[image.Length];
+                image.OpenReadStream().Read(audio.Image, 0, (int)image.Length);
+            }
+
+            audio.Id = count++;
+
+            audios.Add(audio);
+            return View("Index", audios);
+        }
+
+    }
+}
---
+++ AudioAPP/Models/Audio.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace AudioAPP.Models
+{
+    public class Audio
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+        //[Required(ErrorMessage = "Podaj tytuł")]
+        public string Title { get; set; }
+        [HiddenInput]
+        public byte[] Image { get; set; }
+        [HiddenInput]
+        public byte[] Sound { get; set; }
+        public string Description { get; set; }
+
+    }
+}
